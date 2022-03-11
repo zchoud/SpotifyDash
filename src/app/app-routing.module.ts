cd10948e1edef3e9f2bfd1d28cfb63631f5cc13a
zchoud <zchoudhury@myseneca.ca>
2022-03-11 11:09:41 -0500

    working on not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { NewReleasesComponent } from './new-releases/new-releases.component';
 import { AlbumComponent } from './album/album.component';
 import { ArtistDiscographyComponent } from './artist-discography/artist-discography.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   // { [path: '', component: SomeComponent} in order of specific to less
   { path: 'album', component: AlbumComponent },
   { path: 'new-releases', component: NewReleasesComponent },
   { path: 'artist', component: ArtistDiscographyComponent },
+  { path: '*', component: NotFoundComponent },
   { path: '', redirectTo: 'new-releases', pathMatch: 'full'}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
